7fa702093a1259420005cca5ac52b01f0caa4a07
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2024-04-17 02:00:33 +0530

    udpated env

--- src/env.js
+++ src/env.js
@@ -36,7 +36,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
