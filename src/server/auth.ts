402fbd3b94d31f9abe9c7b70bf468a9c417e7cd7
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2024-04-17 01:37:05 +0530

    updated env file

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -49,10 +48,6 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: DrizzleAdapter(db, createTable) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
